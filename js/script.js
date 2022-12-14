bad1a8d66813dbd43b0ad303a4c2c5c67fdb14ca
Esther Camacho <Esther.Camacho@alu.uclm.es>
2022-12-14 13:57:39 +0100

    cambios js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 /* MENU DESPLEGABLE */
 const header = document.querySelector("header");
-window.addEventListener("scroll", function () {
-	header.classList.toggle("sticky", window.scrollY > 100);
-});
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
